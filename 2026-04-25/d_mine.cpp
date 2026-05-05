48c0390d320b0b851247fbf039a51e35168bc920
tomodaore <daichi.m.0501.0816@gmail.com>
2026-05-06 07:22:15 +0900

    d

---
+++ 2026-04-25/d_mine.cpp
@@ -0,0 +1,22 @@
+/*
+完敗
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int main(){
+    ll n,q,c,p;
+    cin >> n >> q;
+    vector<ll> a[n];
+    for(int i = 0;i < n;i++){
+        a[i].push_back(i);
+    }
+    for(ll i = 0;i < q;i++){
+        cin >> c >> p;
+
+    }
+
+}
